161571c7270249aafb2137d66d0ec5b4a13b3e5d
Ben Staton <statonbe@gmail.com>
2020-04-27 10:35:30 -0700

    Create 00-packages.R
    When sourced, this installs any missing packages necessary for the analysis, and loads them into the current session

---
+++ grande-ronde/00-packages.R
@@ -0,0 +1,36 @@
+# INSTALL MISSING PACKAGES IF USER DOES NOT HAVE THOSE REQUIRED AND LOAD THEM
+
+# obtain all installed packages
+pkg_have = rownames(installed.packages())
+
+# the list of all needed packages on CRAN
+pkg_need_cran = c(
+  "scales",     # for transparent colors in plotting
+  "jagsUI",     # for calling JAGS through R
+  "remotes"     # for installing packages off of Github
+  )
+
+# the list of all needed packages on B. Staton's github
+pkg_need_gthb = c(
+  "postpack",   # for clean and easy posterior summarization
+  "StatonMisc"  # for various utilities commonly used by B. Staton
+  )
+
+# install missing packages: CRAN
+for (i in 1:length(pkg_need_cran)) {
+  if (!(pkg_need_cran[i] %in% pkg_have)) {
+    cat("Installing required package (from CRAN):", pkg_need_cran[i], "\n")
+    install.packages(pkg_need_cran[i], quiet = T)
+  }
+}
+
+# install missing packages: Github
+for (i in 1:length(pkg_need_gthb)) {
+  if (!(pkg_need_cran[i] %in% pkg_have)) {
+    cat("Installing required package (from Github):", pkg_need_gthb[i], "\n")
+    remotes::install_github(paste0("bstaton1/", pkg_need_gthb[i]), quiet = T, upgrade = F)
+  }
+}
+
+# load all packages
+junk = suppressWarnings(sapply(c(pkg_need_cran, pkg_need_gthb), function(pkg) do.call("library", list(package = pkg))))
