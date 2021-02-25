4502d4eb3abf8677fd0320fe5874e9de3b74a934
Ben Staton <statonbe@gmail.com>
2021-02-24 16:43:14 -0800

    Delete StatonMisc dependency and add posterior package

--- grande-ronde/00-packages.R
+++ grande-ronde/00-packages.R
@@ -8,13 +8,8 @@ pkg_need_cran = c(
   "scales",     # for transparent colors in plotting
   "jagsUI",     # for calling JAGS through R
   "remotes",    # for installing packages off of Github
-  "stringr"     # for string manipulation
-  )
-
-# the list of all needed packages on B. Staton's github
-pkg_need_gthb = c(
-  "postpack",   # for clean and easy posterior summarization
-  "StatonMisc"  # for various utilities commonly used by B. Staton
+  "stringr",    # for string manipulation
+  "postpack"    # for the majority of posterior summarization
   )
 
 # install missing packages: CRAN
@@ -25,17 +20,15 @@ for (i in 1:length(pkg_need_cran)) {
   }
 }
 
-# install missing packages: Github
-for (i in 1:length(pkg_need_gthb)) {
-  if (!(pkg_need_cran[i] %in% pkg_have)) {
-    cat("Installing required package (from Github):", pkg_need_gthb[i], "\n")
-    remotes::install_github(paste0("bstaton1/", pkg_need_gthb[i]), quiet = T, upgrade = F)
-  }
+# install missing packages: 'posterior' (not yet on CRAN)
+# package contains updated MCMC diagnostics relative to those in postpack
+if (!("posterior" %in% pkg_have)) {
+  install.packages("posterior", repos = c("https://mc-stan.org/r-packages/", getOption("repos")))
 }
 
 # load all packages
 junk = suppressPackageStartupMessages(
   suppressWarnings(
-    sapply(c(pkg_need_cran, pkg_need_gthb), function(pkg) do.call("library", list(package = pkg)))
+    sapply(c(pkg_need_cran, "posterior"), function(pkg) do.call("library", list(package = pkg)))
   )
 )
