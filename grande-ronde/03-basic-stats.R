06172f17e167c9bae3daf249337f277cc1570d3c
Ben Staton <statonbe@gmail.com>
2021-03-12 16:14:05 -0800

    Add more summary calculations in GR analysis

--- grande-ronde/03-basic-stats.R
+++ grande-ronde/03-basic-stats.R
@@ -27,27 +27,52 @@ post = readRDS("outputs/posterior-huggins-Mt.rds")
 # see https://mc-stan.org/posterior/index.html for details on 'posterior'
 # see http://www.stat.columbia.edu/~gelman/research/published/rhat.pdf for details on the revised Rhat/ESS calculations
 post_sub = post_subset(post, c("^N[", "alpha", "beta", "^w[", "^p1[", "^p2[", "sig_epi", "epi"))
-diags = posterior::summarize_draws(posterior::as_draws_df(post_sub), rhat = posterior::rhat, ess_tail = posterior::ess_tail, ess_mean = posterior::ess_mean)
+draws = posterior::as_draws_df(post_sub)
+
+diags1 = posterior::summarize_draws(draws, rhat = posterior::rhat, ess_median = posterior::ess_median)
+diags2 = posterior::summarize_draws(draws, ess_quantile, .args = list(probs = c(0.025, 0.975)))
+diags = merge(diags1, diags2, by = "variable")
 
 # top 10 worst Rhat stats
 head(diags[order(diags[,"rhat"], decreasing = T),], 10)
 
 # top 10 worst ess_tail stats
-head(diags[order(diags[,"ess_tail"]),], 10)
+head(diags[order(diags[,"ess_q2.5"]),], 10)
+head(diags[order(diags[,"ess_q97.5"]),], 10)
 
-# top 10 worst ess_tail stats
-head(diags[order(diags[,"ess_mean"]),], 10)
+# top 10 worst ess_median stats
+head(diags[order(diags[,"ess_median"]),], 10)
 
 # verify the mean(w) is similar across chains
 barplot(t(post_summ(post, "^w", by_chain = T)["mean",,]), beside = T)
 
-# summaries of detection efficiency by species
+# summaries of detection probability by species
 psi = post_summ(post, "^psi[")
-chin_psi = psi["mean",jags_data$X[,"chin"] == 1]
-omyk_psi = psi["mean",jags_data$X[,"chin"] == 0]
+chin_psi = psi["50%",jags_data$X[,"chin"] == 1]
+omyk_psi = psi["50%",jags_data$X[,"chin"] == 0]
 
 round(c(mean(chin_psi), range(chin_psi)), 2)  # mean and range for chinook observations
 round(c(mean(omyk_psi), range(omyk_psi)), 2)  # mean and range for O. mykiss observations
+
+# summaries of abundance by species
+N = post_summ(post, "^N[")
+chin_N = N["50%",jags_data$X[,"chin"] == 1]
+omyk_N = N["50%",jags_data$X[,"chin"] == 0]
+
+round(c(mean(chin_N), range(chin_N)), 0)  # mean and range for chinook observations
+round(c(mean(omyk_N), range(omyk_N)), 0)  # mean and range for O. mykiss observations
+
+# summaries of capture probabilities
+round(quantile(rowMeans(post_subset(post, "^p1", T)), c(0.5, 0.025, 0.975)), 2)
+round(quantile(rowMeans(post_subset(post, "^p2", T)), c(0.5, 0.025, 0.975)), 2)
+
+# calculate posterior of correlation between p1 and p2
+p1 = post_subset(post, "^p1", T)
+p2 = post_subset(post, "^p2", T)
+rho_post = sapply(1:post_dim(post, "saved"), function(i) {
+  cor(p1[i,], p2[i,])
+})
+round(quantile(rho_post, c(0.5, 0.025, 0.975)), 2)
 
 # sd of site-level random effects
 sig_epi = post_summ(post, "sig_epi", digits = 2); sig_epi
